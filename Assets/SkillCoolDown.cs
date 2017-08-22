9248ae0e7ca472d60fbc8a6e130fa1340622411d
2757777 <ikarosuchihaya@ikarosu.local>
2017-08-23 00:41:27 +0900

    add skill buttong & cool down

---
+++ Assets/SkillCoolDown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System;
+using UniRx.Triggers;
+
+public class SkillCoolDown : MonoBehaviour {
+
+	public float cooldown; 
+	public float currentCoolDown;
+	public Image SkillButton;
+	private void Start(){
+		currentCoolDown = 5;
+		this.UpdateAsObservable()
+			.Where(_ => currentCoolDown<cooldown)
+    		.Subscribe(_ => Updateimage());
+	}
+	void Updateimage()
+	{
+		currentCoolDown += 0.1f;
+		SkillButton.fillAmount = currentCoolDown/cooldown; 
+	}
+	void OnClick() 
+	{
+		if(currentCoolDown >= cooldown)
+		{	//TODO: something
+			currentCoolDown = 0;
+		}
+	}
+}
